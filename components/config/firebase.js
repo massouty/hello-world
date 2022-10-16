e2f8c9d813103e4e1ab4f6601ab879bb30a812cd
massouty <massouty@outlook.com>
2022-10-16 18:01:07 +0200

    Create firebase.js
    If applied , this commit will have added file (firebase.js)

---
+++ components/config/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+import Constants from "expo-constants";
+
+// Firebase config
+const firebaseConfig = {
+    apiKey: Constants.manifest.extra.apiKey,
+    authDomain: Constants.manifest.extra.authDomain,
+    projectId: Constants.manifest.extra.projectId,
+    storageBucket: Constants.manifest.extra.storageBucket,
+    messagingSenderId: Constants.manifest.extra.messagingSenderId,
+    appId: Constants.manifest.extra.appId,
+    databaseURL: Constants.manifest.extra.databaseURL
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service (db)
+export const db = getFirestore(app);
+
+// Get a reference to the Firebase auth object
+export const auth = getAuth();
